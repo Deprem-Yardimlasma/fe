e82221dff4fdbf4038e432e5946ef0988d2dc0e4
Mustafa Adıgüzel <mustafa.adiguzel@trendyol.com>
2023-02-07 01:12:53 +0300

    add contact

--- components/global/PageHeader.vue
+++ components/global/PageHeader.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="navbar bg-base-100">
+    <div class="navbar bg-base-100 justify-between">
         <nuxt-link class="btn btn-ghost normal-case text-xl" to="/">toplumsalyardim.org</nuxt-link>
+        <ContentList v-slot="{ list }">
+            <div class="flex gap-4">
+                <nuxt-link v-for="article in list" class="btn btn-ghost normal-case text-xl" :to="article._path">{{ article.title }}</nuxt-link>
+            </div>
+        </ContentList>
     </div>
 </template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="p-4">
+    <ContentDoc class="prose flex flex-col gap-2" />
+  </main>
+</template>
