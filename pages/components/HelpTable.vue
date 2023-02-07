976c130d6aac5008ac68f40102227e52b9231dae
Levent Anil Ozen <leventanilozen@gmail.com>
2023-02-07 23:57:07 +0300

    fix: cosmetic

--- pages/components/HelpTable.vue
+++ pages/components/HelpTable.vue
@@ -23,7 +23,7 @@ const props = defineProps({
               <tr v-for="data in props.data" class="hover">
                   <td>{{ data.address.city }}</td>
                   <td>{{ data.address.town }}</td>
-                  <td>{{ data.address.district }}</td>
+                  <td>{{ data.address.neighborhood }}</td>
                   <td class="max-w-md text-ellipsis overflow-hidden" :class="[data.name === 'Belirtilmemis' ? 'italic font-bold' : '']">{{ data.name }}</td>
                   <td>
                       <nuxt-link :to="`/detail/${data._id}`" class="hover:underline">Detaylı Bilgi</nuxt-link>
